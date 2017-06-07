f08ca135864e60f140c15aaed028b7e9b7dbd75b
johnptmcdonald <johnptmcdonald@gmail.com>
2017-06-06 23:37:46 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,1 @@
+document.getElementById('content').innerHTML = 'Javascript is FUN'
---
+++ tasks.js
@@ -0,0 +1,36 @@
+
+
+console.log("This is your javascript console. If you see this message, you're good to go! Go ahead and run task_one() through task_five() right here");
+
+
+function task_one(){
+	console.log("Task one: use document.getElementById in app.js to select the paragraph that says 'javascript can be hard'. Change it to 'javascript is fun!' ");
+
+}
+
+function task_two(){
+	console.log("Task two: use document.getElementsByClassName to select the title. Change the title's text to 'I LOVE JAVASCRIPT!'");
+}
+
+function task_three(){
+	console.log("Task three: use document.getElementsByTagName to select the body and change the color of all text on the page to yellow");
+	
+}
+
+function task_four(){
+	console.log("Task four: use document.getElementsByTagName to select the sadface image. Change the source to this: 'http://vignette3.wikia.nocookie.net/gtawiki/images/9/99/Smiley_face.png/revision/latest?cb=20150527112010'")
+}
+
+function task_five(){
+	console.log("Task five: Use document.getElementsByTagName to select the body again. We want to give it a background image of this: 'https://lh3.googleusercontent.com/qxTdwNDkgtL961NLxaYCPWR3sP_CqSWrp1OgUaFcNIwTD7_imsTNKsDUi5n1a_mw6SUPTGf1zA=s640-h400-e365'");
+}
+
+///////////// Write your own JS below!  /////////////////////
+
+
+
+
+
+
+
+
